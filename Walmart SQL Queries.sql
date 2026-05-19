531a7ae59614032bbb2f0ed6a481c412e10af2d6
Shital Sarode <sarodeshital@gmail.com>
2026-05-19 23:08:03 +0530

    Update Walmart SQL Queries.sql

--- Walmart SQL Queries.sql
+++ Walmart SQL Queries.sql
@@ -26,7 +26,7 @@ SHOW GLOBAL VARIABLES LIKE 'local_infile';
 SET GLOBAL local_infile = 1;
 
 LOAD DATA LOCAL INFILE 
-'/Users/mohammedshehbazdamkar/Downloads/WalmartSalesData.csv.csv'
+'/Users/shitalsarode/Downloads/WalmartSalesData.csv.csv'
 INTO TABLE sales
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
